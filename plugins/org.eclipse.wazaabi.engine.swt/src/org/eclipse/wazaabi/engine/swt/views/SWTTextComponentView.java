2e45ff67ab2ee3a0eb130a5ea2855180f8afc078
Gregoire de Hemptinne <gregoire.dehemptinne@euranova.eu>
2012-12-10 11:31:05 +0100

    Adding support for EchoChar in TextComponent

--- plugins/org.eclipse.wazaabi.engine.swt/src/org/eclipse/wazaabi/engine/swt/views/SWTTextComponentView.java
+++ plugins/org.eclipse.wazaabi.engine.swt/src/org/eclipse/wazaabi/engine/swt/views/SWTTextComponentView.java
@@ -28,6 +28,7 @@ import org.eclipse.swt.widgets.Widget;
 import org.eclipse.wazaabi.engine.core.editparts.TextComponentEditPart;
 import org.eclipse.wazaabi.engine.core.views.TextComponentView;
 import org.eclipse.wazaabi.mm.core.styles.BooleanRule;
+import org.eclipse.wazaabi.mm.core.styles.StringRule;
 import org.eclipse.wazaabi.mm.core.styles.StyleRule;
 import org.eclipse.wazaabi.mm.core.widgets.TextComponent;
 import org.eclipse.wazaabi.mm.swt.descriptors.SWTDescriptorsPackage;
@@ -119,6 +120,18 @@ public class SWTTextComponentView extends SWTControlView implements
 					((BooleanRule) styleRule).isValue()));
 		} else
 			return super.needReCreateWidgetView(styleRule);
+	}
+	
+	@Override
+	public void updateStyleRule(StyleRule rule) {
+		if (rule == null)
+			return;
+		if (TextComponentEditPart.ECHO_CHAR_PROPERTY_NAME
+				.equals(rule.getPropertyName()) && rule instanceof StringRule){
+			((Text)getSWTControl()).setEchoChar(((StringRule) rule).getValue().charAt(0));
+		} else {
+			super.updateStyleRule(rule);
+		}
 	}
 
 }
