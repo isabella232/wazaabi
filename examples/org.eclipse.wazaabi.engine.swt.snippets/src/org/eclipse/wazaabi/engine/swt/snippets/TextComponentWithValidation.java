372af10d9d3d2bf2344e36ee0848eff7cbb58042
omoises <olivier@generic-concept.com>
2013-01-26 19:20:09 +0100

    changed the TextComponentWitValidation for testing override of validation error text & reassignment of SWT listeners when the widget is recreated (after the change of border)

--- examples/org.eclipse.wazaabi.engine.swt.snippets/src/org/eclipse/wazaabi/engine/swt/snippets/TextComponentWithValidation.java
+++ examples/org.eclipse.wazaabi.engine.swt.snippets/src/org/eclipse/wazaabi/engine/swt/snippets/TextComponentWithValidation.java
@@ -19,6 +19,8 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wazaabi.coderesolution.reflection.java.codelocators.nonosgi.ReflectionJavaHelper;
 import org.eclipse.wazaabi.engine.swt.nonosgi.SWTHelper;
 import org.eclipse.wazaabi.engine.swt.viewers.SWTControlViewer;
+import org.eclipse.wazaabi.mm.core.styles.BooleanRule;
+import org.eclipse.wazaabi.mm.core.styles.CoreStylesFactory;
 import org.eclipse.wazaabi.mm.core.widgets.Container;
 import org.eclipse.wazaabi.mm.core.widgets.CoreWidgetsFactory;
 import org.eclipse.wazaabi.mm.core.widgets.TextComponent;
@@ -38,7 +40,6 @@ public class TextComponentWithValidation {
 		SWTHelper.init();
 		ReflectionJavaHelper.init();
 
-
 		// create the shell
 		Display display = new Display();
 		Shell mainShell = new Shell(display, SWT.SHELL_TRIM);
@@ -50,30 +51,44 @@ public class TextComponentWithValidation {
 
 		// create a container and set its layout
 		Container container = CoreWidgetsFactory.eINSTANCE.createContainer();
+
+		// inject the container into the viewer
+		viewer.setContents(container);
+
 		RowLayoutRule layoutRule = SWTStylesFactory.eINSTANCE
 				.createRowLayoutRule();
 		layoutRule.setPropertyName("layout");
 		container.getStyleRules().add(layoutRule);
 
 		TextComponent text = CoreWidgetsFactory.eINSTANCE.createTextComponent();
-//		text.setErrorText("default error text");
-		
-		EventHandler handler = EDPHandlersFactory.eINSTANCE.createEventHandler();
+
+		// We add a default error text for being sure that the validation text
+		// will override it
+		text.setErrorText("default error text");
+
+		EventHandler handler = EDPHandlersFactory.eINSTANCE
+				.createEventHandler();
 		Validator validator = EDPHandlersFactory.eINSTANCE.createValidator();
-		validator.setUri("urn:java:org.eclipse.wazaabi.engine.swt.snippets.validators.Less5CharsValidator");
+		validator
+				.setUri("urn:java:org.eclipse.wazaabi.engine.swt.snippets.validators.Less5CharsValidator");
 		handler.getExecutables().add(validator);
 		Event event = EDPEventsFactory.eINSTANCE.createEvent();
 		event.setId("core:ui:text:modify");
 		handler.getEvents().add(event);
-		
+
 		text.getHandlers().add(handler);
-		
 
 		// append the button to the container's children list.
 		container.getChildren().add(text);
 
-		// inject the container into the viewer
-		viewer.setContents(container);
+		// We add a boolean rule then we are sure that he swt widget has been
+		// recreated. We want to know if the swt listeners have well be
+		// reassigned
+
+		BooleanRule b = CoreStylesFactory.eINSTANCE.createBooleanRule();
+		text.getStyleRules().add(b);
+		b.setPropertyName("border");
+		b.setValue(true);
 
 		mainShell.open();
 
