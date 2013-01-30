7ec1e35fbe538ae70edb0fda8dbffa795eb0d519
omoises <olivier@generic-concept.com>
2013-01-30 14:41:38 +0100

    bugfix: ContentChangedEventAdapters were not properly detached

--- plugins/org.eclipse.wazaabi.engine.edp/src/org/eclipse/wazaabi/engine/edp/adapters/AbstractPathEventAdapter.java
+++ plugins/org.eclipse.wazaabi.engine.edp/src/org/eclipse/wazaabi/engine/edp/adapters/AbstractPathEventAdapter.java
@@ -12,13 +12,26 @@
 
 package org.eclipse.wazaabi.engine.edp.adapters;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.wazaabi.engine.edp.EDPSingletons;
+import org.eclipse.wazaabi.engine.edp.PathException;
+import org.eclipse.wazaabi.engine.edp.locationpaths.IPointersEvaluator;
+import org.eclipse.wazaabi.mm.edp.EventDispatcher;
 import org.eclipse.wazaabi.mm.edp.events.EDPEventsPackage;
 import org.eclipse.wazaabi.mm.edp.events.PathEvent;
+import org.eclipse.wazaabi.mm.edp.handlers.EventHandler;
 
 public abstract class AbstractPathEventAdapter extends EventAdapter {
+
+	private List<Adapter> adapters = new ArrayList<Adapter>(10);
 
 	public boolean isAdapterForType(Object type) {
 		return false;
@@ -29,12 +42,15 @@ public abstract class AbstractPathEventAdapter extends EventAdapter {
 			switch (notification.getFeatureID(PathEvent.class)) {
 			case EDPEventsPackage.PATH_EVENT__PATH:
 
-				// TODO we should test if the new event is not equals to the previous one
+				// TODO we should test if the new event is not equals to the
+				// previous one
 
 				if (notification.getOldStringValue() != null
 						&& !"".equals(notification.getOldStringValue())) { //$NON-NLS-1$
-					//TODO : we should be able to avoid the duplicate of the current PathEvent
-					// by changing the signature of the xxxEventListening methods
+					// TODO : we should be able to avoid the duplicate of the
+					// current PathEvent
+					// by changing the signature of the xxxEventListening
+					// methods
 					PathEvent oldPathEvent = (PathEvent) EcoreUtil
 							.copy((PathEvent) getTarget());
 					oldPathEvent.setPath(notification.getOldStringValue());
@@ -42,7 +58,7 @@ public abstract class AbstractPathEventAdapter extends EventAdapter {
 				}
 				if (notification.getNewValue() != null
 						&& !"".equals(notification.getNewStringValue())) //$NON-NLS-1$
-					startEventListening((PathEvent)getTarget());
+					startEventListening((PathEvent) getTarget());
 				break;
 			}
 		}
@@ -58,10 +74,6 @@ public abstract class AbstractPathEventAdapter extends EventAdapter {
 			startEventListening((PathEvent) getTarget());
 	}
 
-	protected abstract void startEventListening(PathEvent pathEvent);
-
-	protected abstract void stopEventListening(PathEvent pathEvent);
-
 	public void attachListeners() {
 		if (getTarget() instanceof PathEvent)
 			startEventListening((PathEvent) getTarget());
@@ -71,4 +83,60 @@ public abstract class AbstractPathEventAdapter extends EventAdapter {
 		if (getTarget() instanceof PathEvent)
 			stopEventListening((PathEvent) getTarget());
 	}
+
+	protected abstract Adapter createAdapter(EObject target);
+
+	protected void startEventListening(PathEvent pathEvent) {
+		if (pathEvent.eContainer() instanceof EventHandler
+				&& pathEvent.eContainer().eContainer() instanceof EventDispatcher) {
+			EventDispatcher eventDispatcher = (EventDispatcher) pathEvent
+					.eContainer().eContainer();
+			if (eventDispatcher != null && EDPSingletons.getRegistry() != null
+					&& pathEvent.getPath() != null
+					&& !"".equals(pathEvent.getPath())) { //$NON-NLS-1$
+
+				final IPointersEvaluator pointersEvalutator = getPointersEvaluator();
+
+				if (pointersEvalutator != null) {
+					try {
+						List<?> newPointers = pointersEvalutator
+								.selectPointers(eventDispatcher,
+										pathEvent.getPath());
+
+						Iterator<?> newPointersIterator = newPointers
+								.iterator();
+						while (newPointersIterator.hasNext())
+							adaptPointer(newPointersIterator.next(),
+									pointersEvalutator);
+					} catch (PathException e) {
+						System.err.println(e.getMessage()); // TODO : log that
+					}
+
+				} else {
+					// pointersEvaluator == null ===> log this
+					assert false;
+				}
+			}
+		}
+	}
+
+	protected void stopEventListening(PathEvent pathEvent) {
+		for (Adapter adapter : getAdapters())
+			unsetTarget(adapter);
+		getAdapters().clear();
+	}
+
+	protected void unsetTarget(Adapter adapter) {
+		Notifier target = adapter.getTarget();
+		if (target != null)
+			target.eAdapters().remove(adapter);
+	}
+
+	protected List<Adapter> getAdapters() {
+		return adapters;
+	}
+
+	protected abstract void adaptPointer(Object pointer,
+			final IPointersEvaluator pointersEvalutator);
+
 }
