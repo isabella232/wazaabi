4943ec5f93552c6d6915caf458ab537add210d12
omoises <olivier@generic-concept.com>
2013-01-30 14:41:38 +0100

    bugfix: ContentChangedEventAdapters were not properly detached

--- plugins/org.eclipse.wazaabi.engine.edp/src/org/eclipse/wazaabi/engine/edp/adapters/PropertyChangedEventAdapter.java
+++ plugins/org.eclipse.wazaabi.engine.edp/src/org/eclipse/wazaabi/engine/edp/adapters/PropertyChangedEventAdapter.java
@@ -12,24 +12,15 @@
 
 package org.eclipse.wazaabi.engine.edp.adapters;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
+import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.wazaabi.engine.edp.EDPSingletons;
-import org.eclipse.wazaabi.engine.edp.PathException;
 import org.eclipse.wazaabi.engine.edp.exceptions.OperationAborted;
 import org.eclipse.wazaabi.engine.edp.locationpaths.IPointersEvaluator;
-import org.eclipse.wazaabi.mm.edp.EventDispatcher;
 import org.eclipse.wazaabi.mm.edp.events.Event;
-import org.eclipse.wazaabi.mm.edp.events.PathEvent;
 import org.eclipse.wazaabi.mm.edp.events.PropertyChangedEvent;
-import org.eclipse.wazaabi.mm.edp.handlers.EventHandler;
 
 public class PropertyChangedEventAdapter extends AbstractPathEventAdapter {
 	protected class FeatureAdapter extends AdapterImpl {
@@ -44,9 +35,10 @@ public class PropertyChangedEventAdapter extends AbstractPathEventAdapter {
 							(Event) PropertyChangedEventAdapter.this
 									.getTarget());
 				} catch (OperationAborted e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 					System.err.println(e.getErrorMessage());
+				} catch (RuntimeException ex) {
+					ex.printStackTrace();
 				}
 			}
 		}
@@ -57,108 +49,32 @@ public class PropertyChangedEventAdapter extends AbstractPathEventAdapter {
 
 	};
 
-	private List<FeatureAdapter> featureAdapters = new ArrayList<FeatureAdapter>(
-			10);
-
 	public boolean isAdapterForType(Object type) {
 		return PropertyChangedEvent.class.equals(type);
 	}
 
-	// public void notifyChanged(Notification notification) {
-	// if (notification.getEventType() == Notification.SET) {
-	// switch (notification.getFeatureID(PropertyChangedEvent.class)) {
-	// case EdpPackage.PROPERTY_CHANGED_EVENT__PATH:
-	// // TODO test if the new event is not equals to the previous one
-	// if (notification.getOldValue() != null)
-	// stopPropertyChangedListening((PropertyChangedEvent) notification
-	// .getOldValue());
-	// if (notification.getNewValue() != null)
-	// startPropertyChangedListening((PropertyChangedEvent) notification
-	// .getNewValue());
-	// break;
-	// }
-	// }
-	// super.notifyChanged(notification);
-	// }
-	//
-	// @Override
-	// public void setTarget(Notifier newTarget) {
-	// if (getTarget() instanceof PropertyChangedEvent)
-	// stopPropertyChangedListening((PropertyChangedEvent) getTarget());
-	// super.setTarget(newTarget);
-	// if (getTarget() instanceof PropertyChangedEvent)
-	// startPropertyChangedListening((PropertyChangedEvent) getTarget());
-	// }
+	protected Adapter createAdapter(EObject target) {
+		FeatureAdapter adapter = new FeatureAdapter();
+		((EObject) target).eAdapters().add(adapter);
+		getAdapters().add(adapter);
+		return adapter;
+	}
 
-	@SuppressWarnings("rawtypes")
-	protected void startEventListening(PathEvent pathEvent) {
-		if (pathEvent.eContainer() instanceof EventHandler
-				&& pathEvent.eContainer().eContainer() instanceof EventDispatcher) {
-			EventDispatcher eventDispatcher = (EventDispatcher) pathEvent
-					.eContainer().eContainer();
-			if (eventDispatcher != null && EDPSingletons.getRegistry() != null
-					&& pathEvent.getPath() != null
-					&& !"".equals(pathEvent.getPath())) { //$NON-NLS-1$
-
-				final IPointersEvaluator pointersEvalutator = getPointersEvaluator();
-
-				if (pointersEvalutator != null) {
-					try {
-						List newPointers = pointersEvalutator.selectPointers(
-								eventDispatcher, pathEvent.getPath());
-
-						Iterator newPointersIterator = newPointers.iterator();
-						while (newPointersIterator.hasNext()) {
-							Object pointer = newPointersIterator.next();
-							Object target = pointersEvalutator
-									.getContext(pointer);
-							if (target instanceof EObject) {
-								String propertyName = pointersEvalutator
-										.getPropertyName(pointer);
-								if (propertyName != null) {
-									EStructuralFeature feature = ((EObject) target)
-											.eClass().getEStructuralFeature(
-													propertyName);
-									if (feature != null) {
-										FeatureAdapter adapter = new FeatureAdapter();
-										adapter.setFeature(feature);
-										((EObject) target).eAdapters().add(
-												adapter);
-										featureAdapters.add(adapter);
-									}
-								}
-							}
-						}
-					} catch (PathException e) {
-						System.err.println(e.getMessage()); // TODO : log that
-					}
-
-				} else {
-					// pointersEvaluator == null ===> log this
-					assert false;
+	protected void adaptPointer(Object pointer,
+			final IPointersEvaluator pointersEvalutator) {
+		Object target = pointersEvalutator.getContext(pointer);
+		if (target instanceof EObject) {
+			String propertyName = pointersEvalutator.getPropertyName(pointer);
+			if (propertyName != null) {
+				EStructuralFeature feature = ((EObject) target).eClass()
+						.getEStructuralFeature(propertyName);
+				if (feature != null) {
+					FeatureAdapter adapter = (FeatureAdapter) createAdapter((EObject) target);
+					adapter.setFeature(feature);
 				}
 			}
 		}
 	}
 
-	protected void stopEventListening(PathEvent pathEvent) {
-		Iterator<FeatureAdapter> iterator = featureAdapters.iterator();
-		while (iterator.hasNext()) {
-			FeatureAdapter adapter = iterator.next();
-			Notifier target = adapter.getTarget();
-			if (target != null)
-				target.eAdapters().remove(adapter);
-		}
-		featureAdapters.clear();
-	}
 
-	// public void attachListeners() {
-	// if (getTarget() instanceof PropertyChangedEvent)
-	// startPropertyChangedListening((PropertyChangedEvent) getTarget());
-	// }
-	//
-	// public void detachListeners() {
-	// if (getTarget() instanceof PropertyChangedEvent)
-	// stopPropertyChangedListening((PropertyChangedEvent) getTarget());
-	// }
 }
