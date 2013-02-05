e8206b3927ee618a653bfb5bd9386ec7e00e6d06
omoises <olivier@generic-concept.com>
2013-02-05 10:25:47 +0100

    fixed after merge

--- plugins/org.eclipse.wazaabi.engine.swt/src/org/eclipse/wazaabi/engine/swt/views/SWTWidgetView.java
+++ plugins/org.eclipse.wazaabi.engine.swt/src/org/eclipse/wazaabi/engine/swt/views/SWTWidgetView.java
@@ -28,11 +28,9 @@ import org.eclipse.wazaabi.engine.core.gef.editparts.ListenerList;
 import org.eclipse.wazaabi.engine.core.views.WidgetView;
 import org.eclipse.wazaabi.mm.core.styles.StyleRule;
 import org.eclipse.wazaabi.mm.core.styles.StyledElement;
-<<<<<<< HEAD
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-=======
->>>>>>> 9123a980bdae40de1de18a174e66ab786aea7346
+
 
 public abstract class SWTWidgetView implements WidgetView {
 
