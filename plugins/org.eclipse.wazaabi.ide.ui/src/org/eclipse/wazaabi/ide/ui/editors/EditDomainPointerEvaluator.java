2003cd7df2c889ef08e53023391f6d34806e3329
omoises <olivier@generic-concept.com>
2013-04-29 19:27:01 +0200

    Fixed: when a feature was set using PropertySheetPage , the tree did not reflect the new value

---
+++ plugins/org.eclipse.wazaabi.ide.ui/src/org/eclipse/wazaabi/ide/ui/editors/EditDomainPointerEvaluator.java
@@ -0,0 +1,40 @@
+package org.eclipse.wazaabi.ide.ui.editors;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.gef.EditDomain;
+import org.eclipse.wazaabi.engine.locationpaths.PointersEvaluatorImpl;
+import org.eclipse.wazaabi.ide.ui.editparts.commands.SetFeatureCommand;
+import org.eclipse.wazaabi.ide.ui.propertysheets.eventhandlers.AbstractStyleRuleAction;
+import org.eclipse.wazaabi.mm.edp.Context;
+
+public class EditDomainPointerEvaluator extends PointersEvaluatorImpl {
+
+	private final EditDomain editDomain;
+
+	public EditDomainPointerEvaluator(EditDomain editDomain) {
+		this.editDomain = editDomain;
+	}
+
+	protected void setFeature(EObject target, EStructuralFeature feature,
+			Object value) {
+		// we check if target belongs to the property sheet or to the domain
+		if (target instanceof Context
+				&& ((Context) target)
+						.get(AbstractStyleRuleAction.EDIT_DOMAIN_KEY) == getEditDomain())
+			// the target belongs to the property sheet, no command needed
+			super.setFeature(target, feature, value);
+		else {
+			SetFeatureCommand command = new SetFeatureCommand();
+			command.setFeature(feature);
+			command.setNewValue(value);
+			command.setTarget(target);
+			getEditDomain().getCommandStack().execute(command);
+		}
+	}
+
+	protected EditDomain getEditDomain() {
+		return editDomain;
+	}
+
+}
