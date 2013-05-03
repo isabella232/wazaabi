5bca45b39dd417f7614b92a9549e4c7d97bbdd00
omoises <olivier@generic-concept.com>
2013-05-03 16:09:28 +0200

    Fixed/Improved PointersEvaluatorImpl

--- plugins/org.eclipse.wazaabi.locationpaths/src/org/eclipse/wazaabi/locationpaths/PointersEvaluatorImpl.java
+++ plugins/org.eclipse.wazaabi.locationpaths/src/org/eclipse/wazaabi/locationpaths/PointersEvaluatorImpl.java
@@ -59,17 +59,21 @@ public class PointersEvaluatorImpl implements IPointersEvaluator {
 			if (target.getStep().getAxis() == Axis.VARIABLE) {
 				// TODO: experimental work, need verification
 				Object convertedValue = null;
-				if (newValue instanceof List)
+				if (newValue instanceof List) {
+					if (((List) newValue).size() > 1)
+						convertedValue = converter != null ? converter
+								.convert(newValue) : newValue;
+					else if (((List) newValue).size() == 1)
+						convertedValue = converter != null ? converter
+								.convert(((List) newValue).get(0))
+								: ((List) newValue).get(0);
+					else if (((List) newValue).isEmpty())
+						convertedValue = converter != null ? converter
+								.convert(null) : null;
+				} else
 					convertedValue = converter != null ? converter
 							.convert(newValue) : newValue;
-				// else if (((List) newValue).size() == 1)
-				// convertedValue = converter != null ? converter
-				// .convert(((List) newValue).get(0)) : ((List) newValue)
-				// .get(0);
 
-				else if (newValue == Collections.EMPTY_LIST)
-					convertedValue = converter != null ? converter
-							.convert(null) : null;
 				try {
 					// TODO : not finished, the name of the method will change
 					Method setValueMethod = target
@@ -127,89 +131,10 @@ public class PointersEvaluatorImpl implements IPointersEvaluator {
 										: ((List) newValue).get(0));
 					// TODO : check & test this, not sure at all ==> check side
 					// effects
-					else if (newValue == Collections.EMPTY_LIST)
-						// target.getContext().eSet(feature, null);
+					else if (((List) newValue).isEmpty())
 						setFeature(target.getContext(), feature,
 								converter != null ? converter.convert(null)
 										: null);
-				}
-			}
-		}
-	}
-
-	protected void internalSetValue2(EMFPointer target, Object newValue) {
-		if (target.getStep().getAxis() == Axis.ATTRIBUTE
-				|| target.getStep().getAxis() == Axis.REFERENCE
-				|| target.getStep().getAxis() == Axis.VARIABLE
-				&& target.getStep().getNameTest() != null
-				&& !target.getStep().getNameTest().equals("") //$NON-NLS-1$
-				&& !target.getStep().getNameTest().equals("*")) //$NON-NLS-1$ 
-		{
-
-			if (target.getStep().getAxis() == Axis.VARIABLE) {
-				// TODO: experimental work, need verification
-				Object convertedValue = null;
-				if (newValue instanceof List)
-					convertedValue = newValue;
-
-				// TODO this should go into the validator
-				// else if (newValue == Collections.EMPTY_LIST)
-				// convertedValue = converter != null ? converter.convert(null)
-				// : null;
-				try {
-					// TODO : not finished, the name of the method will change
-					Method setValueMethod = target
-							.getContext()
-							.getClass()
-							.getMethod(
-									"set", new Class[] { String.class, Object.class }); //$NON-NLS-1$
-					if (setValueMethod != null)
-						setValueMethod.invoke(target.getContext(),
-								new Object[] { target.getStep().getNameTest(),
-										convertedValue });
-
-				} catch (SecurityException e) {
-					e.printStackTrace();
-				} catch (NoSuchMethodException e) {
-					// Nothing to do here
-				} catch (IllegalArgumentException e) {
-					// Nothing to do here
-				} catch (IllegalAccessException e) {
-					// Nothing to do here
-				} catch (InvocationTargetException e) {
-					// Nothing to do here
-				}
-				return;
-			}
-			EStructuralFeature feature = target.getContext().eClass()
-					.getEStructuralFeature(target.getStep().getNameTest());
-			if (feature != null) {
-				switch (target.getStep().getAxis()) {
-				case Axis.ATTRIBUTE:
-					if (!(feature instanceof EAttribute))
-						return;
-					break;
-				case Axis.REFERENCE:
-					if (!(feature instanceof EReference))
-						return;
-					break;
-				default:
-					return;
-				}
-
-				// TODO : not enough, need to manage different cases List->List,
-				// iteration on List, etc...
-				if (newValue instanceof List) {
-					if (feature.isMany())
-						setFeature(target.getContext(), feature, newValue);
-					else if (((List) newValue).size() == 1)
-						setFeature(target.getContext(), feature,
-								((List) newValue).get(0));
-					// TODO : check & test this, not sure at all ==> check side
-					// effects
-					else if (newValue == Collections.EMPTY_LIST)
-						// target.getContext().eSet(feature, null);
-						setFeature(target.getContext(), feature, null);
 				}
 			}
 		}
@@ -264,7 +189,7 @@ public class PointersEvaluatorImpl implements IPointersEvaluator {
 		if (!(pointer instanceof EMFPointer))
 			throw new ClassCastException(
 					"Pointer is not an instance of EMFPointer."); //$NON-NLS-1$
-		internalSetValue2((EMFPointer) pointer, newValue);
+		internalSetValue((EMFPointer) pointer, newValue, null);
 	}
 
 	// TODO very temporary, will be replaced.
@@ -302,7 +227,7 @@ public class PointersEvaluatorImpl implements IPointersEvaluator {
 								((List) newValue).get(0));
 					// TODO : check & test this, not sure at all ==> check side
 					// effects
-					else if (newValue == Collections.EMPTY_LIST)
+					else if (((List) newValue).isEmpty())
 						return resolveConverter(feature, null);
 				}
 			}
