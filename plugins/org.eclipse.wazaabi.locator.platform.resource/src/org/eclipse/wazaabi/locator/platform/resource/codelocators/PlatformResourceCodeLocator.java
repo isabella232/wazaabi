8b9074978307b32c6e4aa3b41340daaad2e1d00c
omoises <olivier@generic-concept.com>
2013-06-14 15:40:10 +0200

    fixed locator.platform.resource plugin according to newly designed service factory

--- plugins/org.eclipse.wazaabi.locator.platform.resource/src/org/eclipse/wazaabi/locator/platform/resource/codelocators/PlatformResourceCodeLocator.java
+++ plugins/org.eclipse.wazaabi.locator.platform.resource/src/org/eclipse/wazaabi/locator/platform/resource/codelocators/PlatformResourceCodeLocator.java
@@ -26,58 +26,63 @@ import org.eclipse.wazaabi.engine.edp.coderesolution.AbstractCodeDescriptor;
 import org.eclipse.wazaabi.engine.edp.coderesolution.AbstractCodeLocator;
 import org.eclipse.wazaabi.locator.platform.resource.codedescriptors.ResourceCodeDescriptor;
 
-
 public class PlatformResourceCodeLocator extends AbstractCodeLocator {
+
+	public static final String FACTORY_ID = PlatformResourceCodeLocator.class
+			.getName();
 
 	static private final String URI_PREFIX = "platform:/plugin/"; //$NON-NLS-1$ 
 	static private final String LANGUAGE = "java"; //$NON-NLS-1$
 
-	private static final Pattern PATTERN = Pattern
-			.compile("platform:/plugin/([^/]+)/([^\\?]+)(\\?language=(\\w+))?"); //$NON-NLS-1$ 
+	private static final Pattern PATTERN = Pattern.compile(URI_PREFIX
+			+ "([^/]+)/([^\\?]+)(\\?language=(\\w+))?"); //$NON-NLS-1$ 
 	private static final int PATTERN_BUNDLE = 1;
 	private static final int PATTERN_PATH = 2;
 	private static final int PATTERN_LANGUAGE = 4;
-	
-	private final ResourceSet rset = new ResourceSetImpl(); 
-	
+
+	private final ResourceSet rset = new ResourceSetImpl();
+
 	public AbstractCodeDescriptor resolveCodeDescriptor(String uri) {
 		Matcher m = PATTERN.matcher(uri);
 		if (m.matches())
-			return new ResourceCodeDescriptor(m.group(PATTERN_BUNDLE), m.group(PATTERN_PATH));
+			return new ResourceCodeDescriptor(m.group(PATTERN_BUNDLE),
+					m.group(PATTERN_PATH));
 		return null;
 	}
 
 	public InputStream getResourceInputStream(String uri) throws IOException {
-	    URI u = URI.createURI(uri);
-	    if (u != null)
-	        return rset.getURIConverter().createInputStream(u);
+		URI u = URI.createURI(uri);
+		if (u != null)
+			return rset.getURIConverter().createInputStream(u);
 		return null;
 	}
 
-	public boolean isCodeLocatorFor(String uri) {
-		if (uri == null)
+	@Override
+	public Object createComponent(Object callingContext, Object model,
+			Object creationHint) {
+		if (model instanceof String) {
+			Matcher m = PATTERN.matcher((String) model);
+			if (m.matches())
+				return new ResourceCodeDescriptor(m.group(PATTERN_BUNDLE),
+						m.group(PATTERN_PATH));
+		}
+		return null;
+	}
+
+	@Override
+	public String getFactoryID() {
+		return FACTORY_ID;
+	}
+
+	@Override
+	public boolean isFactoryFor(Object callingContext, Object model) {
+		if (!(model instanceof String))
 			return false;
-		Matcher m = PATTERN.matcher(uri);
+		Matcher m = PATTERN.matcher((String) model);
 		if (m.matches()) {
 			String language = m.group(PATTERN_LANGUAGE);
 			return language == null || LANGUAGE.equals(language);
 		}
 		return false;
-	}
-
-	public String getFullPath(String prefix, String relativePath, Object context) {
-		if (relativePath != null && relativePath.startsWith(URI_PREFIX))
-			return relativePath;
-		if (prefix != null && prefix.startsWith(URI_PREFIX)) {
-			if (relativePath != null && !relativePath.startsWith("//")) {
-				if (relativePath.startsWith("/"))
-					relativePath = relativePath.substring(1);
-				if (prefix.endsWith("/"))
-					return prefix + relativePath;
-				else
-					return prefix + '/' + relativePath;
-			}
-		}
-		return null;
 	}
 }
