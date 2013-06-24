39a988727f8c8034a4103c416fb3c89673e4c19a
omoises <olivier@generic-concept.com>
2013-06-24 18:11:45 +0200

    Created SWTCommonsHelper, refactored SWTHelper(s) (in Rap & SWT engines). Created a new SWTTextComponent inheriting from the SWT one. Created a new snippet TextComponentInAForm

---
+++ plugins/org.eclipse.wazaabi.engine.swt.forms/src/org/eclipse/wazaabi/engine/swt/forms/nonosgi/SWTFormsHelper.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Olivier Moises
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Olivier Moises- initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.wazaabi.engine.swt.forms.nonosgi;
+
+import org.eclipse.wazaabi.engine.core.views.factories.WidgetViewFactory;
+import org.eclipse.wazaabi.engine.edp.Registry;
+import org.eclipse.wazaabi.engine.edp.nonosgi.EDPHelper;
+import org.eclipse.wazaabi.engine.swt.commons.nonosgi.SWTCommonsHelper;
+import org.eclipse.wazaabi.engine.swt.forms.views.SWTFormsWidgetViewFactory;
+
+public class SWTFormsHelper {
+
+	public static void init(Registry registry) {
+
+		EDPHelper.addService(registry, WidgetViewFactory.class,
+				new SWTFormsWidgetViewFactory());
+		SWTCommonsHelper.init(registry);
+
+	}
+
+}
