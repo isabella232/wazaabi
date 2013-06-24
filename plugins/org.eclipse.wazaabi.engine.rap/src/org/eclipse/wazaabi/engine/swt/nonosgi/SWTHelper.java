922a236e7f9b42e9445f8bf2133dc04225c7d6e8
omoises <olivier@generic-concept.com>
2013-06-24 16:16:24 +0200

    Created SWTCommonsHelper, refactored SWTHelper(s) (in Rap & SWT engines). Created a new SWTTextComponent inheriting from the SWT one. Created a new snippet TextComponentInAForm

--- plugins/org.eclipse.wazaabi.engine.rap/src/org/eclipse/wazaabi/engine/swt/nonosgi/SWTHelper.java
+++ plugins/org.eclipse.wazaabi.engine.rap/src/org/eclipse/wazaabi/engine/swt/nonosgi/SWTHelper.java
@@ -12,39 +12,17 @@
 
 package org.eclipse.wazaabi.engine.swt.nonosgi;
 
-import org.eclipse.wazaabi.engine.core.celleditors.factories.CellEditorFactory;
-import org.eclipse.wazaabi.engine.core.editparts.factories.EditPartFactory;
-import org.eclipse.wazaabi.engine.core.nonosgi.CoreHelper;
-import org.eclipse.wazaabi.engine.core.stylerules.factories.StyleRuleManagerFactory;
-import org.eclipse.wazaabi.engine.core.views.factories.WidgetViewFactory;
 import org.eclipse.wazaabi.engine.edp.Registry;
 import org.eclipse.wazaabi.engine.edp.events.EventAdapterFactory;
-import org.eclipse.wazaabi.engine.edp.events.EventHandlerAdapterFactory;
 import org.eclipse.wazaabi.engine.edp.nonosgi.EDPHelper;
 import org.eclipse.wazaabi.engine.rap.events.RapEventAdapterFactory;
-import org.eclipse.wazaabi.engine.swt.commons.celleditors.factories.SWTCellEditorFactory;
-import org.eclipse.wazaabi.engine.swt.commons.editparts.SWTEditPartFactory;
-import org.eclipse.wazaabi.engine.swt.commons.editparts.stylerules.factories.SWTSpecificStyleRuleManagerFactory;
-import org.eclipse.wazaabi.engine.swt.commons.events.SWTEventHandlerAdapterFactory;
-import org.eclipse.wazaabi.engine.swt.commons.views.SWTWidgetViewFactory;
-import org.eclipse.wazaabi.mm.swt.styles.SWTStylesPackage;
+import org.eclipse.wazaabi.engine.swt.commons.nonosgi.SWTCommonsHelper;
 
 public class SWTHelper {
 
 	public synchronized static void init(Registry registry) {
-		CoreHelper.init(registry);
+		SWTCommonsHelper.init(registry);
 		EDPHelper.addService(registry, EventAdapterFactory.class,
 				new RapEventAdapterFactory());
-		EDPHelper.addService(registry, EventHandlerAdapterFactory.class,
-				new SWTEventHandlerAdapterFactory());
-		EDPHelper.addService(registry, EditPartFactory.class,
-				new SWTEditPartFactory());
-		EDPHelper.addService(registry, WidgetViewFactory.class,
-				new SWTWidgetViewFactory());
-		EDPHelper.addService(registry, StyleRuleManagerFactory.class,
-				new SWTSpecificStyleRuleManagerFactory());
-		EDPHelper.addService(registry, CellEditorFactory.class,
-				new SWTCellEditorFactory());
-		SWTStylesPackage.eINSTANCE.eClass();
 	}
 }
--- plugins/org.eclipse.wazaabi.engine.swt.forms/src/org/eclipse/wazaabi/engine/swt/forms/views/SWTFormsWidgetViewFactory.java
+++ plugins/org.eclipse.wazaabi.engine.swt.forms/src/org/eclipse/wazaabi/engine/swt/forms/views/SWTFormsWidgetViewFactory.java
@@ -3,15 +3,16 @@ package org.eclipse.wazaabi.engine.swt.forms.views;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.wazaabi.engine.core.gef.EditPart;
+import org.eclipse.wazaabi.engine.swt.commons.views.SWTTextComponentView;
 import org.eclipse.wazaabi.mm.core.widgets.CoreWidgetsPackage;
 
-public class FormsWidgetViewFactory extends
+public class SWTFormsWidgetViewFactory extends
 		org.eclipse.wazaabi.engine.swt.commons.views.SWTWidgetViewFactory {
 
 	// private final Logger logger = LoggerFactory
 	// .getLogger(SWTWidgetViewFactory.class);
 
-	public static final String FACTORY_ID = FormsWidgetViewFactory.class
+	public static final String FACTORY_ID = SWTFormsWidgetViewFactory.class
 			.getName();
 
 	@Override
@@ -21,11 +22,11 @@ public class FormsWidgetViewFactory extends
 				&& ((EditPart) model).getModel() instanceof EObject) {
 			EClass eClass = ((EObject) ((EditPart) model).getModel()).eClass();
 
-			if (eClass == CoreWidgetsPackage.Literals.CONTAINER) {
-				System.out.println("container returned by "
-						+ this.getClass().getName());
+			if (eClass == CoreWidgetsPackage.Literals.TEXT_COMPONENT)
+				return new SWTTextComponentView();
+			if (eClass == CoreWidgetsPackage.Literals.CONTAINER)
 				return new SWTContainerView();
-			}
+			
 			return super.createComponent(callingContext, model, creationHint);
 		}
 		return null;
