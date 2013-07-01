afb89906791c1aa4dc789c6c6efe3b58b8a0d6ab
omoises <olivier@generic-concept.com>
2013-07-01 15:37:08 +0200

    Added : SWTFormsEditPartsFactory in order to use forms ContainerEditPart.class when necessary

--- plugins/org.eclipse.wazaabi.engine.core/src/org/eclipse/wazaabi/engine/core/editparts/ContainerEditPart.java
+++ plugins/org.eclipse.wazaabi.engine.core/src/org/eclipse/wazaabi/engine/core/editparts/ContainerEditPart.java
@@ -25,8 +25,6 @@ import org.eclipse.wazaabi.mm.core.widgets.CoreWidgetsPackage;
 public class ContainerEditPart extends AbstractComponentEditPart {
 
 	public static final String TITLE = "title"; // $NON-NLS-1$
-	public static final String HEADER_IMAGE = "header-image"; // $NON-NLS-1$
-	public static final String FORM_DECORATE_FORM_HEADING = "form-decorate-form-heading"; // $NON-NLS-1$
 
 	// TODO : layout is not a platform specific rule ?
 	public static final String LAYOUT_PROPERTY_NAME = "layout"; //$NON-NLS-1$
@@ -123,8 +121,6 @@ public class ContainerEditPart extends AbstractComponentEditPart {
 		refreshUniqueStyleRule(LAYOUT_PROPERTY_NAME);
 		refreshUniqueStyleRule(LAYOUT_DATA_PROPERTY_NAME);
 		refreshUniqueStyleRule(TITLE);
-		refreshUniqueStyleRule(HEADER_IMAGE);
-		refreshUniqueStyleRule(FORM_DECORATE_FORM_HEADING);
 		refreshUniqueStyleRule(TextComponentEditPart.HORIZONTAL_SCROLLBAR_PROPERTY_NAME);
 		refreshUniqueStyleRule(TextComponentEditPart.VERTICAL_SCROLLBAR_PROPERTY_NAME);
 	}
