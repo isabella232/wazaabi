fbcd16181dc270a42687a5b96df256fab8f27389
Olivier <olivier@generic-concept.com>
2014-03-04 10:35:32 +0100

    Removed @Override which lead to compile errors

--- plugins/org.eclipse.wazaabi.ide.propertysheets.forms/src/org/eclipse/wazaabi/ide/propertysheets/forms/viewers/FormBasedBindingsViewer.java
+++ plugins/org.eclipse.wazaabi.ide.propertysheets.forms/src/org/eclipse/wazaabi/ide/propertysheets/forms/viewers/FormBasedBindingsViewer.java
@@ -10,47 +10,39 @@ public class FormBasedBindingsViewer implements PropertySection {
 
 	private Composite container;
 
-	@Override
 	public void addTargetChangeListener(TargetChangeListener listener) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void removeTargetChangeListener(TargetChangeListener listener) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void refresh() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void setInput(Object input) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void createControls(Composite parent) {
 		container = new Composite(parent, SWT.NONE);
 	}
 
-	@Override
 	public void dispose() {
 		if (container != null && !container.isDisposed())
 			container.dispose();
 	}
 
-	@Override
 	public String getLabel() {
 		return "Binding"; //$NON-NLS-1$
 	}
 
-	@Override
 	public Control getControl() {
 		return container;
 	}
